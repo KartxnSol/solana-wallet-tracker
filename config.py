9ec53a5282a37768e9a4dec4206c74389a947b14
KartxnSol <abee7788@proton.me>
2025-08-07 06:15:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,30 +1,15 @@
 import os
 from dotenv import load_dotenv
-import psycopg2
 
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")  # From Railway Postgres
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-def get_connection():
-    return psycopg2.connect(DATABASE_URL, sslmode='require')
+# Helius
+HELIUS_API_KEY = os.getenv("HELIUS_API_KEY")
+HELIUS_BASE_URL = f"https://api.helius.xyz/v0"
 
 def create_tables():
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            user_id BIGINT PRIMARY KEY
-        );
-    """)
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS wallets (
-            id SERIAL PRIMARY KEY,
-            user_id BIGINT REFERENCES users(user_id),
-            address TEXT NOT NULL
-        );
-    """)
-    conn.commit()
-    cur.close()
-    conn.close()
+    from database import create_tables as ct
+    ct()
