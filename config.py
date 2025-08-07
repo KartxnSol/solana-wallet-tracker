029415ef49c86534bb068a1e1e586d3d29b0373a
KartxnSol <abee7788@proton.me>
2025-08-07 07:01:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-# Helius
 HELIUS_API_KEY = os.getenv("HELIUS_API_KEY")
-HELIUS_BASE_URL = f"https://api.helius.xyz/v0"
 
 def create_tables():
     from database import create_tables as ct
