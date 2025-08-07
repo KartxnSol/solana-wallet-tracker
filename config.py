661fd9ecbf4090deaa170df9b802500f2773be31
KartxnSol <abee7788@proton.me>
2025-08-07 07:13:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 import os
 from dotenv import load_dotenv
+from database import create_tables
 
+# Load environment variables from .env file
 load_dotenv()
 
+# Telegram bot token
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+# PostgreSQL connection string
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Helius API key
 HELIUS_API_KEY = os.getenv("HELIUS_API_KEY")
